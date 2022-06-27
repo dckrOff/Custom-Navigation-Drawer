4f804f023b0ba3f40d772065eb9f27645e55c7f6
dckrOff <azamatotaboev7621@gmail.com>
2022-06-27 17:56:07 +0500

    first commit

---
+++ app/src/main/java/com/a1tech/custom_navigation_drawer/MainActivity.java
@@ -0,0 +1,130 @@
+package com.a1tech.custom_navigation_drawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+import com.shrikanthravi.customnavigationdrawer2.data.MenuItem;
+import com.shrikanthravi.customnavigationdrawer2.widget.SNavigationDrawer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Global Declaration
+    SNavigationDrawer sNavigationDrawer;
+    Class fragmentClass;
+    public static Fragment fragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Inside onCreate()
+
+        sNavigationDrawer = findViewById(R.id.navigationDrawer);
+
+        //Creating a list of menu Items
+
+        List<MenuItem> menuItems = new ArrayList<>();
+
+        //Use the MenuItem given by this library and not the default one.
+        //First parameter is the title of the menu item and then the second parameter is the image which will be the background of the menu item.
+
+        menuItems.add(new MenuItem("News",R.drawable.news_bg));
+        menuItems.add(new MenuItem("Feed",R.drawable.feed_bg));
+        menuItems.add(new MenuItem("Messages",R.drawable.message_bg));
+        menuItems.add(new MenuItem("Music",R.drawable.music_bg));
+
+        //then add them to navigation drawer
+
+        sNavigationDrawer.setMenuItemList(menuItems);
+        fragmentClass = NewsFragment.class;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).replace(R.id.frameLayout, fragment).commit();
+        }
+
+
+
+        //Listener to handle the menu item click. It returns the position of the menu item clicked. Based on that you can switch between the fragments.
+
+        sNavigationDrawer.setOnMenuItemClickListener(new SNavigationDrawer.OnMenuItemClickListener() {
+            @Override
+            public void onMenuItemClicked(int position) {
+                System.out.println("Position "+position);
+
+                switch (position){
+                    case 0:{
+                        fragmentClass = NewsFragment.class;
+                        break;
+                    }
+                    case 1:{
+                        fragmentClass = FeedFragment.class;
+                        break;
+                    }
+                    case 2:{
+                        fragmentClass = MessagesFragment.class;
+                        break;
+                    }
+                    case 3:{
+                        fragmentClass = MusicFragment.class;
+                        break;
+                    }
+
+                }
+
+                //Listener for drawer events such as opening and closing.
+                sNavigationDrawer.setDrawerListener(new SNavigationDrawer.DrawerListener() {
+
+                    @Override
+                    public void onDrawerOpened() {
+
+                    }
+
+                    @Override
+                    public void onDrawerOpening(){
+
+                    }
+
+                    @Override
+                    public void onDrawerClosing(){
+                        System.out.println("Drawer closed");
+
+                        try {
+                            fragment = (Fragment) fragmentClass.newInstance();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+                        if (fragment != null) {
+                            FragmentManager fragmentManager = getSupportFragmentManager();
+                            fragmentManager.beginTransaction().setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out).replace(R.id.frameLayout, fragment).commit();
+
+                        }
+                    }
+
+                    @Override
+                    public void onDrawerClosed() {
+
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        System.out.println("State "+newState);
+                    }
+                });
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/a1tech/custom_navigation_drawer/MusicFragment.java
@@ -0,0 +1,24 @@
+package com.a1tech.custom_navigation_drawer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+public class MusicFragment extends Fragment {
+
+    public MusicFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_music, container, false);
+    }
+
+}
